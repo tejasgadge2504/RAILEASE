ccdad2d2df85f10a274c342412eeb299076aa992
tejasgadge2504 <tejasgadge903@gmail.com>
2024-01-27 17:53:25 +0530

    updated code pushed

--- lib/RequestSent.dart
+++ lib/RequestSent.dart
@@ -6,12 +6,13 @@ class RequestSent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(13.0),
       color: Colors.white,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
+
             color: Colors.tealAccent,
             padding: const EdgeInsets.all(20.0),
             child: Text(
@@ -19,34 +20,43 @@ class RequestSent extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text('Please collect the HardCopy of the Concession Form From the Railway Concession Counter',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text('Thank you For using. See you Again',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text('For any Queries Contact',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('Feedback'),
-                ),
-              ],
+          SingleChildScrollView(
+
+              child: Column(
+
+                children: [
+
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text('Please collect the HardCopy of the Concession Form From the Railway Concession Counter',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text('Thank you For using. See you Again',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text('For any Queries Contact : railease@ves.ac.in',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),),
+                      ),
+                    ],
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pop(context);
+                  //   },
+                  //   child: Text('Feedback'),
+                  // ),
+                ],
+              ),
             ),
-          ),
+
         ],
       ),
     );
--- lib/welcome_Screen.dart
+++ lib/welcome_Screen.dart
@@ -21,64 +21,66 @@ class Welcome_Screen extends StatelessWidget{
         width: width,
 
         
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 40,),
-            Padding(
-              padding: const EdgeInsets.all(0.0),
-              child: Image.asset(applogo_bluebg ,height: height*0.2,),
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Image.asset(applogo_bluebg ,height: height*0.2,),
+              ),
 
-            Text('RAILEASE',style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500), ),
-            Text('Railway Concession Automation at VESIT',style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500), ),
-            SizedBox(height: height*0.05),
-            Text('WELCOME',style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),  ),
+              Text('RAILEASE',style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500), ),
+              Text('Railway Concession Automation at VESIT',style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500), ),
+              SizedBox(height: height*0.05),
+              Text('WELCOME',style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),  ),
 
-            Text('Student',style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400), ),
-            SizedBox(height: 65,),
-            Text('Please Kindly Login or SignUp on the App',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),textAlign: TextAlign.center,),
-            SizedBox(height: height*0.06,),
-            Row(
-              children: [
-                SizedBox(width: width*0.1,),
-                Expanded(child: ElevatedButton(onPressed: (){
-                  Navigator.of(context).push(_createRoute());
-                }, child: Text('LOGIN'),style: ElevatedButton.styleFrom(
-                  side: BorderSide(color: tAppbar_color),
-                  padding: EdgeInsets.symmetric(vertical: 25),
-                ),)),
-                SizedBox(width: width*0.05,),
-                Expanded(child: OutlinedButton(onPressed: (){
-                  Navigator.of(context).push(_regRoute());
-                }, child: Text('New User?\nRegister Here'),
-                  style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.black),
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                ),)),
-                SizedBox(width: width*0.1,),
+              Text('Student',style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400), ),
+              SizedBox(height: 65,),
+              Text('Please Kindly Login or SignUp on the App',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),textAlign: TextAlign.center,),
+              SizedBox(height: height*0.06,),
+              Row(
+                children: [
+                  SizedBox(width: width*0.1,),
+                  Expanded(child: ElevatedButton(onPressed: (){
+                    Navigator.of(context).push(_createRoute());
+                  }, child: Text('LOGIN'),style: ElevatedButton.styleFrom(
+                    side: BorderSide(color: tAppbar_color),
+                    padding: EdgeInsets.symmetric(vertical: 25),
+                  ),)),
+                  SizedBox(width: width*0.05,),
+                  Expanded(child: OutlinedButton(onPressed: (){
+                    Navigator.of(context).push(_regRoute());
+                  }, child: Text('New User?\nRegister Here'),
+                    style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.black),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),)),
+                  SizedBox(width: width*0.1,),
 
-              ],
-            ),
-        SizedBox(height: height*0.03,),
-            Text('OR'),
-            SizedBox(height: height*0.03,),
-
-            SizedBox(
-              width: width*0.5,
-              child: ElevatedButton(onPressed: (){
-                Navigator.of(context).push(_adminRoute());
-              },
-                child: Text('ADMIN\'s Corner',style: TextStyle(
-              ),),
-                style: ElevatedButton.styleFrom(
-                side: BorderSide(color: tAppbar_color),
-                padding: EdgeInsets.symmetric(vertical: 20),
-              ),)
-
-            ),
                 ],
               ),
+          SizedBox(height: height*0.03,),
+              Text('OR'),
+              SizedBox(height: height*0.03,),
+
+              SizedBox(
+                width: width*0.5,
+                child: ElevatedButton(onPressed: (){
+                  Navigator.of(context).push(_adminRoute());
+                },
+                  child: Text('ADMIN\'s Corner',style: TextStyle(
+                ),),
+                  style: ElevatedButton.styleFrom(
+                  side: BorderSide(color: tAppbar_color),
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                ),)
+
+              ),
+                  ],
+                ),
+        ),
 
       ),
     );
