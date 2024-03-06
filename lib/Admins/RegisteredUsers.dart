1c7f136eaf94bcf46e23a329a432c592988640bb
tejasgadge2504 <tejasgadge903@gmail.com>
2024-03-07 00:35:09 +0530

    tejas pushed the approved db logic

--- lib/Admins/RegisteredUsers.dart
+++ lib/Admins/RegisteredUsers.dart
@@ -1,6 +1,9 @@
+// import 'dart:js';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import '';
 
 class RegUsers extends StatefulWidget {
   const RegUsers({Key? key}) : super(key: key);
@@ -13,34 +16,36 @@ class _RegUsersState extends State<RegUsers> {
 
   Query dbRef = FirebaseDatabase.instance.ref().child('Reg Users');
   DatabaseReference reference = FirebaseDatabase.instance.ref().child('Reg Users');
+  DatabaseReference approvedUsersReference = FirebaseDatabase.instance.ref().child('Approved Users');
+
 
   Widget listItem({required Map student}) {
     return Container(
       margin: const EdgeInsets.all(10),
       padding: const EdgeInsets.all(10),
       height: 150,
-      color: Colors.amberAccent,
+      color: Colors.deepPurpleAccent,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             student['Name'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500,color: Colors.white),
           ),
           const SizedBox(
             height: 5,
           ),
           Text(
             student['VES_ID'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400,color: Colors.white),
           ),
           const SizedBox(
             height: 5,
           ),
           Text(
             student['Branch'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400,color: Colors.white),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
@@ -48,13 +53,15 @@ class _RegUsersState extends State<RegUsers> {
             children: [
               GestureDetector(
                 onTap: () {
-                  // Navigator.push(context, MaterialPageRoute(builder: (_) => UpdateRecord(studentKey: student['key'])));
+
+              //EDIT LOGIC
+
                 },
                 child: Row(
                   children: [
                     Icon(
                       Icons.edit,
-                      color: Theme.of(context).primaryColor,
+                      color: Colors.yellow,
                     ),
                   ],
                 ),
@@ -64,17 +71,49 @@ class _RegUsersState extends State<RegUsers> {
               ),
               GestureDetector(
                 onTap: () {
-                  reference.child(student['key']).remove();
+
+                  //APPROVE APPLICATION
+                  // Move the approved user to the 'Approved Users' database
+                  reference.child(student['Key']).remove(); // Remove from 'Reg Users'
+                  approvedUsersReference.child(student['Key']).set(student); // Add to 'Approved Users'
+
                 },
                 child: Row(
                   children: [
                     Icon(
-                      Icons.delete,
-                      color: Colors.red[700],
+                      Icons.add,
+                      color: Colors.lightGreenAccent,
+                      weight: Checkbox.width,
+                      size: 30,
                     ),
                   ],
                 ),
               ),
+
+              const SizedBox(
+                width: 6,
+              ),
+              GestureDetector(
+                onTap: () {
+                  // Navigator.push(
+                  //   context as BuildContext,
+                  //   MaterialPageRoute(builder: (context) =>viewMore),
+                  // );
+                  //SEEN THE DETAILS
+
+                },
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.white,
+                      weight: Checkbox.width,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ),
+
             ],
           )
         ],
@@ -86,7 +125,7 @@ class _RegUsersState extends State<RegUsers> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Fetching data'),
+          title: const Text('Registered users'),
         ),
         body: Container(
           height: double.infinity,
@@ -94,8 +133,9 @@ class _RegUsersState extends State<RegUsers> {
             query: dbRef,
             itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
 
+
               Map student = snapshot.value as Map;
-              student['key'] = snapshot.key;
+              student['Key'] = snapshot.key;
 
               return listItem(student: student);
 
